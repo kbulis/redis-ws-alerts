368ed280041988789c18231a159196be9c1ad8eb
kbulis <kirk.d.bulis@gmail.com>
2017-11-25 23:43:23 -0800

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# redis-ws-alerts
-Alerting clients over websockets of events posted to redis lists.
+# redis-ws-alerts [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+Alerting clients over express-bound websockets of events posted to redis lists.
+
+## License
+
+ISC © [Kirk Bulis](http://github.com/kbulis)
+
+[npm-image]: https://badge.fury.io/js/redis-ws-alerts.svg
+[npm-url]: https://npmjs.org/package/redis-ws-alerts
+[travis-image]: https://travis-ci.org/kbulis/redis-ws-alerts.svg?branch=master
+[travis-url]: https://travis-ci.org/kbulis/redis-ws-alerts
+[daviddm-image]: https://david-dm.org/kbulis/redis-ws-alerts.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/kbulis/redis-ws-alerts
---
+++ lib/index.ts
@@ -0,0 +1,262 @@
+import * as express from 'express';
+import * as storage from 'redis';
+import * as sockets from 'ws';
+
+/**
+ * ConnectionServer
+ * 
+ * Maintains websocket client connections for subsequently pushed alert
+ * messages raised by a connected-content source (like redis list pop).
+ * 
+ * Example usage:
+ * 
+ * ...
+ * ...
+ * ...
+ * 
+ */
+export class ConnectionServer {
+
+  private server: sockets.Server;
+  private lookup: any[] = [];
+
+  private handleNewConnect = (connected: any, req: express.Request): void => {
+    connected.trackingId = 'c' + (Math.floor(Math.random() * 9000000) + 1000000);
+    connected.key = '';
+
+    this.lookup.push(connected);
+
+    connected.on('message', (message: string) => {
+      try {
+        const identify = JSON.parse(message || '{}');
+  
+        if (identify.event === 'identify') {
+          if (identify.pcode && identify.ucode) {
+            if (connected.key !== '') {
+              if (connected.key !== 'alert/' + identify.pcode + '-' + identify.ucode) {
+                if (this.onClose) {
+                  try {
+                    this.onClose(connected.key, this.lookup.some((tracked) => {
+                      return tracked.key === connected.key && tracked.trackingId !== connected.trackingId;
+                    }));
+                  }
+                  catch (eX) {
+                  }
+                }
+              }
+              else {
+                return;
+              }
+            }
+
+            connected.key = 'alert/' + identify.pcode + '-' + identify.ucode;
+
+            connected.send(JSON.stringify({
+              message: 'you are now connected',
+            }));
+
+            if (this.onReady) {
+              try {
+                this.onReady(connected.key, !this.lookup.some((tracked) => {
+                  return tracked.key === connected.key && tracked.trackingId !== connected.trackingId;
+                }));
+              }
+              catch (eX) {
+              }
+            }
+          }
+        }
+      }
+      catch (eX) {
+      }
+    });
+  
+    connected.on('error', (err: any) => {
+      if (this.onError) {
+        try {
+          this.onError(err);
+        }
+        catch (eX) {
+        }
+      }
+    });
+
+    connected.on('close', () => {
+      this.lookup = this.lookup.filter((tracked) => {
+        return tracked.trackingId !== connected.trackingId;
+      });
+
+      if (this.onClose) {
+        try {
+          this.onClose(connected.key, this.lookup.some((tracked) => {
+            return tracked.key === connected.key;
+          }));
+        }
+        catch (eX) {
+        }
+      }
+    });
+  };
+
+  pushNotification = (key: string, message: any): void => {
+    for (const connected of this.lookup) {
+      if (connected.key === key) {
+        try {
+          connected.send(JSON.stringify(message));
+        }
+        catch (eX) {
+          if (this.onError) {
+            try {
+              this.onError(eX);
+            }
+            catch (oX) {
+            }
+          }
+        }
+      }
+    }
+  };
+
+  onReady: (key: string, isInitial: boolean) => void;
+
+  onClose: (key: string, remaining: boolean) => void;
+
+  onError: (err: Error) => void;
+
+  onStart: () => void;
+
+  close = (): void => {
+    try {
+      this.server.close();
+    }
+    catch (eX) {
+    }
+  };
+
+  constructor(app: express.Application, port: string) {
+    this.server = new sockets.Server({
+      server: app.listen(port)
+    });
+
+    this.server.on('connection', this.handleNewConnect);
+
+    this.server.on('error', (err: any) => {
+      if (this.onError) {
+        try {
+          this.onError(err);
+        }
+        catch (eX) {
+        }
+      }
+    });
+    
+    this.server.on('listening', () => {
+      if (this.onStart) {
+        try {
+          this.onStart();
+        }
+        catch (eX) {
+        }
+      }
+    });
+  }
+
+}
+
+/**
+ * ConnectedContent
+ * 
+ * Watches a redis sink list and pops alert messages from the queue named
+ * on the top of the sink. Once a client to redis is established, we watch
+ * the sink as a queue by blocking on a list pop until an alert queue key
+ * is pushed. Once pushed, the key name is popped off the sink of keys,
+ * then the alert message is popped of the queue named by the key.
+ * 
+ * When used with a connection-server, you can push the alert message from
+ * the named queue to a connected client with the same queue name. 
+ * 
+ * Example usage:
+ * 
+ * ...
+ * ...
+ * ...
+ * 
+ */
+export class ConnectedContent {
+
+  private client: storage.RedisClient;
+  private listen: string;
+
+  private consumeFromQueue = (): void => {
+    this.client.blpop(this.listen, 0, (bad, key: any) => {
+      if (!bad && key) {
+        if (key.length === 2) {
+          this.client.lpop(key[1], (err, message: any) => {
+            if (!err && message) {
+              const next = JSON.parse(message);
+              if (next.event === 'alert') {
+                if (this.onFetch) {
+                  try {
+                    this.onFetch(key[1], {
+                      ...next,
+                    });
+                  }
+                  catch (eX) {
+                  }
+                }
+              }                  
+            }
+          });
+        }
+      }
+      
+      this.consumeFromQueue();
+    });
+  }
+
+  onFetch: (key: string, message: any) => void;
+
+  onError: (err: Error) => void;
+
+  onStart: () => void;
+
+  close = (): void => {
+    try {
+      this.client.quit();
+    }
+    catch (eX) {
+    }
+  };
+
+  constructor(connect: string, listen: string) {
+    this.listen = listen;
+    this.client = storage.createClient(connect, {
+      retry_strategy: (options: storage.RetryStrategyOptions) => {
+        return 5000;
+      }
+    });
+
+    this.client.on('error', (err: any) => {
+      if (this.onError) {
+        try {
+          this.onError(err);
+        }
+        catch (eX) {
+        }
+      }
+    });
+
+    this.client.on('ready', () => {
+      if (this.onStart) {
+        try {
+          this.onStart();
+        }
+        catch (eX) {
+        }
+      }
+
+      this.consumeFromQueue();
+    });
+  }
+
+}
